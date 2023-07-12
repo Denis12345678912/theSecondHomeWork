7e1f8554fa71bfa3007f8760eb92ffcb42730b67
Den <no.den2018@yandex.ru>
2023-07-12 12:58:47 +0500

    Всё решили

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Рандомное число: " + number);
+int theSecondFigure = number / 10;
+theSecondFigure %=  10;
+Console.WriteLine($"Вторая цифра, числа {number} - {theSecondFigure}");
+
